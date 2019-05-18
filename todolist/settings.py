c6d74dbe7be8051b45fea53bd82ee4b4511636c8
kgpyo <deletenickname@daum.net>
2019-05-19 00:30:56 +0900

    수정

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9krd1%#f9$0@n-b1=05!m1%^h^&p$ull3)gp8(+*m3psuixkmt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,4 +119,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
